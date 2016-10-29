a4f3cd120599276bcbc6317454624b689e2aa4c1
jonathasmedina <emaildojonathas@gmail.com>
2016-10-29 16:18:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jonathas/computgraf/Greeting.java
@@ -0,0 +1,46 @@
+package com.example.jonathas.computgraf;
+
+import java.util.Date;
+
+/**
+ * Created by Asus on 01/02/2016.
+ */
+
+
+public class Greeting {
+
+    private int id;
+    private String titulo;
+    private String texto;
+    private Date data;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+
+    /* private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+*/
+}
+
